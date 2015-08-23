00ec97fda89f8d76f49a01e50f925653e6944a97
Akash Nimare <svnitakash@gmail.com>
2015-08-24 02:33:45 +0530

    Update git.js

--- git.js
+++ git.js
@@ -1,22 +1,21 @@
 var github = require('octonode');
 var simpleGit = require('simple-git');
 var user_login = {
-  username: '<USERNAME',
-  password: '<PASSWORD>'
+  username: process.argv[2],
+  password: process.argv[3]
 };
 var client = github.client(user_login);
 var ghme   = client.me();
 client.post('/user/repos', {}, function (err, status, body, headers) {
   ghme.repo({
-    "name": process.argv[2]
+    "name": process.argv[4]
   }, function () {
       require('simple-git')()
      .init()
      .add('./*')
      .commit("first commit!")
-     .addRemote('origin', 'git@github.com:' + user_login.username + '/' + process.argv[2] + '.git')
+     .addRemote('origin', 'git@github.com:' + process.argv[2] + '/' + process.argv[4] + '.git')
      .push('origin', 'master');
 
   } );
 });
-
